b455506d318c7fe03f83e10e69b1fe1f264dd023
Unknown <clementserrano3@gmail.com>
2018-10-18 09:26:41 +0200

    Setup des 3 projets : spring, angular et android

---
+++ projetcinema/src/main/java/com/epul/info/projetcinema/ProjetcinemaApplication.java
@@ -0,0 +1,12 @@
+package com.epul.info.projetcinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetcinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetcinemaApplication.class, args);
+	}
+}
---
+++ projetcinema/src/test/java/com/epul/info/projetcinema/ProjetcinemaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.epul.info.projetcinema;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetcinemaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
