12e6ee9c9a5d76ffaea41f3c1746af01d83fe490
Unknown <natjolivet@hotmail.fr>
2019-01-09 09:49:33 +0100

    start angular

--- angularcinema/src/app/app.module.ts
+++ angularcinema/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FilmListComponent } from './film-list/film-list.component';
+import { FilmItemComponent } from './film-item/film-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FilmListComponent,
+    FilmItemComponent
   ],
   imports: [
     BrowserModule
