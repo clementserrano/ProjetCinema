e69d64cd46b2ab8415f04e849659c7edef506396
Unknown <natjolivet@hotmail.fr>
2019-02-01 16:22:40 +0100

    Services done

--- angularcinema/src/app/service/categorie.service.ts
+++ angularcinema/src/app/service/categorie.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Categorie} from '../model/categorie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategorieService {
+  url = 'http://localhost:8080/projetcinema/categorie';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getCategories(): Observable<Categorie[]> {
+    return this.http.get<Categorie[]>(this.url);
+  }
+
+  getCategorie(code: string): Observable<Categorie> {
+    return this.http.get<Categorie>(this.url + '/' + code);
+  }
 }
