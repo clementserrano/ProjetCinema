47c0efafd76e64a19d21c043e1d999a81fce7893
Unknown <clementserrano3@gmail.com>
2019-02-02 19:00:49 +0100

    get categories et realisateurs

--- angularcinema/src/app/film-add-update/film-add-update.component.ts
+++ angularcinema/src/app/film-add-update/film-add-update.component.ts
@@ -4,6 +4,9 @@ import {Film} from '../model/film';
 import {Categorie} from '../model/categorie';
 import {Realisateur} from '../model/realisateur';
 import {Personnage} from '../model/personnage';
+import {RealisateurService} from '../service/realisateur.service';
+import {PersonnageService} from '../service/personnage.service';
+import {CategorieService} from '../service/categorie.service';
 
 @Component({
   selector: 'app-film-add-update',
@@ -19,10 +22,40 @@ export class FilmAddUpdateComponent implements OnInit {
   datasource;
   displayedColumns: string[] = ['acteur', 'role'];
 
-  constructor(filmService: FilmService) {
+  constructor(private filmService: FilmService, private realisateurService: RealisateurService,
+              private categorieService: CategorieService, private personnageService: PersonnageService) {
   }
 
   ngOnInit() {
+    this.getRealisateurs();
+    this.getCategories();
   }
 
+  getRealisateurs(): void {
+    this.realisateurService.getRealisateurs().subscribe(realisateurs => {
+      this.realisateurs = realisateurs;
+    });
+  }
+
+  getCategories(): void {
+    this.categorieService.getCategories().subscribe(categories => {
+      this.categories = categories;
+    });
+  }
+
+  /*
+  this.dataSource = new MatTableDataSource(this.films);
+      this.dataSource.sortingDataAccessor = (item, property) => {
+        switch (property) {
+          case 'realisateur':
+            return item.realisateur.prenRea + ' ' + item.realisateur.nomRea;
+          case 'categorie':
+            return item.categorie.libelleCat;
+          default:
+            return item[property];
+        }
+      };
+      this.dataSource.sort = this.sort;
+    });
+   */
 }
