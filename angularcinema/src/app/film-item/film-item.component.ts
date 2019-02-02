f67e47e3c981ce795d4c481a93462c5043841164
Unknown <natjolivet@hotmail.fr>
2019-02-02 15:29:31 +0100

    affichages infos film sur sa page

--- angularcinema/src/app/film-item/film-item.component.ts
+++ angularcinema/src/app/film-item/film-item.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FilmService } from '../service/film.service';
 import {ActivatedRoute} from '@angular/router';
+import {MatTableDataSource} from '@angular/material';
+import {Film} from '../model/film';
+import {RealisateurService} from '../service/realisateur.service';
+import {Realisateur} from '../model/realisateur';
 
 @Component({
   selector: 'app-film-item',
@@ -8,11 +12,28 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./film-item.component.css']
 })
 export class FilmItemComponent implements OnInit {
+  film: Film;
+  realisateur: Realisateur;
   public noFilm;
-  constructor(private filmService: FilmService, private route: ActivatedRoute) { }
+  constructor(private filmService: FilmService, private realisateurService: RealisateurService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.noFilm = this.route.snapshot.paramMap.get('noFilm');
+    this.getFilm();
   }
+  /*TODO faire comme dans film-list.component pour récup les infos du film et les afficher ensuite*/
+  getFilm(): void {
+    this.filmService.getFilm(this.noFilm).subscribe(film => {
+      this.film = film;
+    });
+    this.getRealisateur();
+  }
+
+  getRealisateur(): void {
+    this.realisateurService.getRealisateur(this.film.realisateur.noRea).subscribe(realisateur => {
+      this.realisateur = realisateur;
+    });
+  }
+
 
 }
