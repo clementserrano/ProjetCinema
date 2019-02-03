93c749cebe8545f8908c5774f7d458f2cad7a72f
Unknown <natjolivet@hotmail.fr>
2019-02-03 15:21:12 +0100

    amélioration affichage

--- angularcinema/src/app/film-item/film-item.component.ts
+++ angularcinema/src/app/film-item/film-item.component.ts
@@ -31,4 +31,9 @@ export class FilmItemComponent implements OnInit {
     });
   }
 
+  convertMinsToHrsMins(minutes): string {
+    const h = Math.floor(minutes / 60);
+    const m = minutes % 60;
+    return h + 'h' + m + 'm';
+  }
 }
